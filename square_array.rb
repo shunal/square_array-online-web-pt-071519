820e1f9066b8b85af56edda43cd03b3436e31cd4
Steven Hunal <stevehunal@gmail.com>
2019-07-19 16:52:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 require "pry"
 def square_array(array)
  array.each do |number|
-   new_number = number ** 2
    new_array = []
+   new_number = number ** 2
    new_array << new_number
  end
  new_array
